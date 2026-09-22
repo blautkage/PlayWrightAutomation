52f32aa5c760e475699462d62fb1c8821a88dd01
blautkage <daniel.comolli@gmail.com>
2026-09-22 15:38:00 +0200

    Apiscripts

---
+++ tests/NetworkSecurityTest.spec.js
@@ -0,0 +1,20 @@
+const { test,expect } = require('@playwright/test');
+let email = "da9lco@gmail.com";
+ 
+ 
+test('@QW Security test request intercept', async ({ page }) => {
+ 
+    //login and reach orders page
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill(email);
+    await page.locator("#userPassword").fill("Pudding1!");
+    await page.locator("#login").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+ 
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
+        route => route.continue({ url: 'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=621661f884b053f6765465b6' }))
+    await page.locator("button:has-text('View')").first().click();
+    await expect(page.locator("p").last()).toHaveText("You are not authorize to view this order");
+})
